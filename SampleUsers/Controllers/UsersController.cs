e62b42cd4140169ed3d21b41636fd25e7666e020
Erdem Özer <erdemcemozer@hotmail.com>
2019-07-11 12:15:36 +0300

    Add delete action.

--- SampleUsers/Controllers/UsersController.cs
+++ SampleUsers/Controllers/UsersController.cs
@@ -7,28 +7,22 @@ namespace SampleUsers.Controllers
 {
     public class UsersController : Controller
     {
-        private static List<User> _users;
-
-        public UsersController()
-        {
-            _users = new List<User>();
-            _users.Add(new User
+        private static List<User> _users = new List<User>(){
+            new User
             {
                 Id = 1,
                 Name = "Mahmoud",
                 Surname = "Slama",
-                IsChecked = false
-            });
-
-            _users.Add(new User
+                IsChecked = true
+            },
+            new User
             {
                 Id = 2,
                 Name = "Oguz",
                 Surname = "Taha",
                 IsChecked = true
-            });
-        }
-
+            }
+        };
         // GET: Users
         public ActionResult Index()
         {
@@ -38,19 +32,29 @@ namespace SampleUsers.Controllers
         [HttpPost]
         public ActionResult AddNewUser(User user)
         {
-
-            int id = _users[_users.Count - 1].Id + 1;
+            int id = 1;
+            if (_users.Count > 0)
+            {
+                id = _users[_users.Count - 1].Id + 1;
+            }
 
             Models.User newUser = new User()
             {
                 Id = id,
                 Name = user.Name,
                 Surname = user.Surname,
-                IsChecked = user.IsChecked
+                IsChecked = true
             };
 
             _users.Add(newUser);
 
+            return View("Index", _users);
+        }
+
+        [HttpGet]
+        public ActionResult DeleteById(int id)
+        {
+            _users.Remove(_users.Find(x => x.Id == id));
             return View("Index", _users);
         }
     }
